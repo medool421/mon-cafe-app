918d7526d03d5cdda455365b76ac12eaee6e5af5
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2025-10-20 15:34:17 +0100

    init

---
+++ app/_layout.jsx
@@ -0,0 +1,20 @@
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <Stack />;
+}
