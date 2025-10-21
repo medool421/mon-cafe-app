a7871b2ad7d1d6c3a6218a3fbe4ac0ed68c36983
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2025-10-21 17:01:11 +0100

    fix : babel.js

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,6 @@
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
+export default function RootLayout() {  
 
   return <Stack />;
-}
+};
