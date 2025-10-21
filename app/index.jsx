ec2e7de98df27ef21682d9c0a7ba7a6ae805703a
AYYOUB <elqasryayyoub@gmail.com>
2025-10-21 11:02:49 +0100

    create branch El Qasry

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function Index() {
   return (
     <View>
-      <Text>Index</Text>
+      <Text>Inpm start ndex</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
