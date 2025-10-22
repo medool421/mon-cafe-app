8f9447a12a0681c8e9613e7a4fa19749a240deb9
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2025-10-22 20:43:11 +0100

    Add home screen with bg image and Jaro font

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,9 @@
-import { Stack } from "expo-router";
-
-export default function RootLayout() {  
-
-  return <Stack />;
-};
+import {  Stack } from "expo-router";
+import {} from "expo-status-bar"
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
