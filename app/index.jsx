120bab474ed5d2c1b9ab1690a087a266ceb646ce
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2025-10-24 02:22:25 +0100

    Icon and splash icon added with the menu button with gradient bg

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 import React from "react";
 import {ImageBackground} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import { StatusBar } from "expo-status-bar";
-import { useFonts, Jaro_400Regular } from '@expo-google-fonts/jaro';
+import RootLayout from "./_layout";
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 
 export default function Home() {
-  let [fontsLoaded] = useFonts({Jaro_400Regular,});
 
-  if (!fontsLoaded) {
-  return null;
-}
   return (
 
     <>
@@ -21,6 +20,21 @@ export default function Home() {
               <View style={{ marginTop: 490}}>
               <Text style={{color: '#fff5ec', textAlign: "center", margin: 45, fontFamily: "Jaro_400Regular" ,fontSize:29,  textShadowColor: "black",textShadowOffset: { width: -2, height: 2 },textShadowRadius: 2}}>Fuel your passion. Master your brew. Experience coffee like never before with BrewTime.</Text>
               </View>
+              <View style={styles.container}>
+
+      <Link href="/about" asChild>
+        <Pressable>
+             <LinearGradient
+                colors={['#CB7D2F', '#653E17']}
+                start={{ x: 0, y: 0.5 }}
+                end={{ x: 1, y: 0.5 }}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>Go to Menu   ➔</Text>
+              </LinearGradient>
+        </Pressable>
+      </Link>
+    </View>
           </ImageBackground>
           
 
@@ -32,3 +46,24 @@ export default function Home() {
 
  
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+
+  },
+  button: {
+    paddingVertical: 7,
+    paddingHorizontal: 25,
+    borderRadius: 19,
+     borderWidth: 3,
+    borderColor: 'white',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 26,
+    fontFamily: "Jaro_400Regular"
+  },
+});
